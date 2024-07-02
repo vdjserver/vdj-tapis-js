40ef2813adefe2cb72b22553df2f11a86494a590
Scott Christley <scott.christley@utsouthwestern.edu>
2024-07-02 17:59:06 -0500

    utility functions for ADC queries

---
+++ adc_mongo_query.js
@@ -0,0 +1,303 @@
+'use strict';
+
+//
+// adc_mongo_query.js
+// Utility functions for processing ADC queries for Mongo database
+//
+// VDJServer
+// http://vdjserver.org
+//
+// Copyright (C) 2024 The University of Texas Southwestern Medical Center
+//
+// Author: Scott Christley <scott.christley@utsouthwestern.edu>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+var ADCMongoQuery = {};
+module.exports = ADCMongoQuery;
+
+var tapisV3 = require('vdj-tapis-js/tapisV3');
+var config = tapisV3.config;
+
+// escape strings for regex, double \\ for restheart
+var escapeString = function(text) {
+    var encoded = text.replace(/\*/g, '\\\\\*');
+    encoded = encoded.replace(/\+/g, '\\\\\+');
+    return encoded;
+}
+
+// Clean data record
+// Remove any internal fields
+ADCMongoQuery.cleanRecord = function(record) {
+    if (record['_id']) delete record['_id'];
+    if (record['_etag']) delete record['_etag'];
+    return record;
+}
+
+
+// Construct mongodb query based upon the ADC filters parameters. The
+// filters parameter is a JSON object that can be any number of nested
+// levels, so we recursively construct the query.
+
+ADCMongoQuery.constructQueryOperation = function(airr, schema, filter, error) {
+    var context = 'ADCMongoQuery.constructQueryOperation';
+
+    if (!filter['op']) {
+        error['message'] = 'missing op';
+        return null;
+    }
+    if (!filter['content']) {
+        error['message'] = 'missing content';
+        return null;
+    }
+
+    var content = filter['content'];
+
+    // TODO: do we need to handle value being an array when a single value is expected?
+    // TODO: validate queryable field names?
+
+    // determine type from schema, default is string
+    var content_type = null;
+    var content_properties = airr.specForQueryField(schema, content['field']);
+    if (content_properties) content_type = content_properties['type'];
+    if (!content_properties) {
+        config.log.info(context, content['field'] + ' is not found in AIRR schema.');
+    } else content_type = content_properties['type'];
+
+    // if not in schema then maybe its a custom field
+    // so use the same type as the value.
+    if (!content_type) content_type = typeof content['value'];
+    //config.log.info(context, 'type: ' + content_type);
+
+    // verify the value type against the field type
+    // stringify the value properly for the query
+    var content_value = undefined;
+    if (content['value'] != undefined) {
+        if (content['value'] instanceof Array) {
+            // we do not bother checking the types of array elements
+            content_value = JSON.stringify(content['value']);
+        } else {
+            // if the field is an array
+            // then check if items are basic type
+            if (content_type == 'array') {
+                if (content_properties && content_properties['items'] && content_properties['items']['type'])
+                    content_type = content_properties['items']['type'];
+            }
+
+            switch(content_type) {
+            case 'integer':
+            case 'number':
+                if (((typeof content['value']) != 'integer') && ((typeof content['value']) != 'number')) {
+                    error['message'] = "value has wrong type '" + typeof content['value'] + "', should be integer or number.";
+                    return null;
+                }
+                content_value = content['value'];
+                break;
+            case 'boolean':
+                if ((typeof content['value']) != 'boolean') {
+                    error['message'] = "value has wrong type '" + typeof content['value'] + "', should be boolean.";
+                    return null;
+                }
+                content_value = content['value'];
+                break;
+            case 'string':
+                if ((typeof content['value']) != 'string') {
+                    error['message'] = "value has wrong type '" + typeof content['value'] + "', should be string.";
+                    return null;
+                }
+                content_value = '"' + content['value'] + '"';
+                break;
+            default:
+                error['message'] = "unsupported content type: " + content_type;
+                return null;
+            }
+        }
+    }
+
+    // query operators
+    switch(filter['op']) {
+    case '=':
+        if (content['field'] == undefined) {
+            error['message'] = 'missing field for = operator';
+            return null;
+        }
+        if (content_value == undefined) {
+            error['message'] = 'missing value for = operator';
+            return null;
+        }
+        return '{"' + content['field'] + '":' + content_value + '}';
+
+    case '!=':
+        if (content['field'] == undefined) {
+            error['message'] = 'missing field for != operator';
+            return null;
+        }
+        if (content_value == undefined) {
+            error['message'] = 'missing value for != operator';
+            return null;
+        }
+        return '{"' + content['field'] + '": { "$ne":' + content_value + '}}';
+
+    case '<':
+        if (content['field'] == undefined) {
+            error['message'] = 'missing field for < operator';
+            return null;
+        }
+        if (content_value == undefined) {
+            error['message'] = 'missing value for < operator';
+            return null;
+        }
+        return '{"' + content['field'] + '": { "$lt":' + content_value + '}}';
+
+    case '<=':
+        if (content['field'] == undefined) {
+            error['message'] = 'missing field for <= operator';
+            return null;
+        }
+        if (content_value == undefined) {
+            error['message'] = 'missing value for <= operator';
+            return null;
+        }
+        return '{"' + content['field'] + '": { "$lte":' + content_value + '}}';
+
+    case '>':
+        if (content['field'] == undefined) {
+            error['message'] = 'missing field for > operator';
+            return null;
+        }
+        if (content_value == undefined) {
+            error['message'] = 'missing value for > operator';
+            return null;
+        }
+        return '{"' + content['field'] + '": { "$gt":' + content_value + '}}';
+
+    case '>=':
+        if (content['field'] == undefined) {
+            error['message'] = 'missing field for >= operator';
+            return null;
+        }
+        if (content_value == undefined) {
+            error['message'] = 'missing value for >= operator';
+            return null;
+        }
+        return '{"' + content['field'] + '": { "$gte":' + content_value + '}}';
+
+    case 'contains':
+        if (content_type != 'string') {
+            error['message'] = "'contains' operator only valid for strings";
+            return null;
+        }
+        if (content['field'] == undefined) {
+            error['message'] = "missing field for 'contains' operator";
+            return null;
+        }
+        if (content_value == undefined) {
+            error['message'] = "missing value for 'contains' operator";
+            return null;
+        }
+        return '{"' + content['field'] + '": { "$regex":' + escapeString(content_value) + ', "$options": "i"}}';
+
+    case 'is': // is missing
+    case 'is missing':
+        if (content['field'] == undefined) {
+            error['message'] = "missing field for 'is missing' operator";
+            return null;
+        }
+        return '{"' + content['field'] + '": { "$exists": false } }';
+
+    case 'not': // is not missing
+    case 'is not missing':
+        if (content['field'] == undefined) {
+            error['message'] = "missing field for 'is not missing' operator";
+            return null;
+        }
+        return '{"' + content['field'] + '": { "$exists": true } }';
+
+    case 'in':
+        if (content_value == undefined) {
+            error['message'] = "missing value for 'in' operator";
+            return null;
+        }
+        if (! (content['value'] instanceof Array)) {
+            error['message'] = "value for 'in' operator is not an array";
+            return null;
+        }
+        if (content['field'] == undefined) {
+            error['message'] = "missing field for 'in' operator";
+            return null;
+        }
+        return '{"' + content['field'] + '": { "$in":' + content_value + '}}';
+
+    case 'exclude':
+        if (content_value == undefined) {
+            error['message'] = "missing value for 'exclude' operator";
+            return null;
+        }
+        if (! (content['value'] instanceof Array)) {
+            error['message'] = "value for 'exclude' operator is not an array";
+            return null;
+        }
+        if (content['field'] == undefined) {
+            error['message'] = "missing field for 'exclude' operator";
+            return null;
+        }
+        return '{"' + content['field'] + '": { "$nin":' + content_value + '}}';
+
+    case 'and': {
+        if (! (content instanceof Array)) {
+            error['message'] = "content for 'and' operator is not an array";
+            return null;
+        }
+        if (content.length < 2) {
+            error['message'] = "content for 'and' operator needs at least 2 elements";
+            return null;
+        }
+
+        var exp_list = [];
+        for (var i = 0; i < content.length; ++i) {
+            var exp = ADCMongoQuery.constructQueryOperation(content[i], error);
+            if (exp == null) return null;
+            exp_list.push(exp);
+        }
+        return '{ "$and":[' + exp_list + ']}';
+    }
+
+    case 'or': {
+        if (! (content instanceof Array)) {
+            error['message'] = "content for 'or' operator is not an array";
+            return null;
+        }
+        if (content.length < 2) {
+            error['message'] = "content for 'or' operator needs at least 2 elements";
+            return null;
+        }
+
+        var exp_list = [];
+        for (var i = 0; i < content.length; ++i) {
+            var exp = ADCMongoQuery.constructQueryOperation(content[i], error);
+            if (exp == null) return null;
+            exp_list.push(exp);
+        }
+        return '{ "$or":[' + exp_list + ']}';
+    }
+
+    default:
+        error['message'] = 'unknown operator in filters: ' + filter['op'];
+        return null;
+    }
+
+    // should not get here
+    //return null;
+}
